5ef9b653c554fe47109c16b5cd063a38ebcbd6be
itsivang <itsivang@gmail.com>
2022-10-08 23:20:14 +0800

    aa

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args){
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
+            //haha git is fun
         }
     }
 }
